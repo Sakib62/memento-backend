e0901d4034d9aff4e05002fecbb3eeec67f2cbd4
sakib62 <sakib62.coder@gmail.com>
2025-02-04 10:00:37 +0600

    add: created database connection and user and auth table

---
+++ migrations/20250203124933_create_auth_table.js
@@ -0,0 +1,16 @@
+export function up(knex) {
+  return knex.schema.createTable("auth", (table) => {
+    table.increments("id").primary();
+    table
+      .integer("userId")
+      .unsigned()
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE");
+    table.string("password").notNullable();
+  });
+}
+
+export function down(knex) {
+  return knex.schema.dropTable("auth");
+}
