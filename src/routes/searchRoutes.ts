a9612c28f7f662e96554c55156a8cc7637d1b709
sakib62 <sakib62.coder@gmail.com>
2025-02-19 19:14:05 +0600

    protect search route using auth middleware

--- src/routes/searchRoutes.ts
+++ src/routes/searchRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import SearchController from '../controllers/searchController';
+import { authMiddleware } from '../middlewares/authMiddleware';
 
 const router = Router();
 
-router.get('/', SearchController.searchAll);
+router.get('/', authMiddleware, SearchController.searchAll);
 
 export default router;
